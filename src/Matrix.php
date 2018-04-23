0f76d5beb5f91b3a689980136cbf5ba6e499da69
Unknown <szymon.gackowski@gmail.com>
2018-04-24 00:43:12 +0200

    Dodana macierz losowa

--- src/Matrix.php
+++ src/Matrix.php
@@ -7,7 +7,7 @@ use Phpml\Math\Matrix as MatrixBase;
 class Matrix extends MatrixBase implements \ArrayAccess{
 	
 	/**
-	 * Generate matrix of $rows x $columns size filled 
+	 * Generate matrix of size $rows x $columns filled 
 	 * with zeros
 	 * @param  [type] $rows    [description]
 	 * @param  [type] $columns [description]
@@ -19,12 +19,52 @@ class Matrix extends MatrixBase implements \ArrayAccess{
 		return new self($fill);
 	}
 
+	/**
+	 * Generate matrix of size $rows x $columns filled 
+	 * with random (float) numbers ranging 
+	 * from $min to $max
+	 * @param  int  	$rows    number of rows
+	 * @param  int  	$columns number of columns
+	 * @param  float 	$min     random range min
+	 * @param  float 	$max     random range max
+	 * @return Matrix            new matrix
+	 */
+	public static function random(int $rows, int $columns, float $min = 0, float $max = 1): self 
+	{
+
+		if($min >= $max){
+			throw new \Exception("Wrong min and max values");
+		}
+
+		$fill = array_fill(0, $rows, array_fill(0, $columns, 0));
+		for($i=0; $i<count($fill); $i++){
+			for($j=0; $j<count($fill[0]); $j++){
+				$fill[$i][$j] = static::rand_float($min, $max);
+			}
+		}
+
+		return new self($fill);
+	}
+
+	/**
+	 * Generate random floating point number in provided range
+	 * @param  integer $st_num  [description]
+	 * @param  integer $end_num [description]
+	 * @param  integer $mul     [description]
+	 * @return [type]           [description]
+	 */
+	protected static function rand_float($st_num=0,$end_num=1,$mul=1000000)
+	{
+		if ($st_num>$end_num) return false;
+		return mt_rand($st_num*$mul,$end_num*$mul)/$mul;
+	}
+
 	// ------------------ ArrayAccess methods ----------------------
 
 	public function offsetExists($offset) 
-    { 
-    	return isset($this->toArray()[$offset]);
-    } 
+	{ 
+		return isset($this->toArray()[$offset]);
+	} 
 
 	public function offsetSet($offset, $value) 
 	{ 
@@ -38,7 +78,7 @@ class Matrix extends MatrixBase implements \ArrayAccess{
 
 	public function offsetUnset($offset) 
 	{ 
-		
+
 	} 
 
 }

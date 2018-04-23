ef412a414c3aa1663ed9d0a2c4c502de4b5b1f00
Unknown <szymon.gackowski@gmail.com>
2018-04-24 00:11:11 +0200

    Service provider goły

---
+++ src/NeuralServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Gacek\Neural;
+
+use Illuminate\Support\ServiceProvider;
+
+class NeuralServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
